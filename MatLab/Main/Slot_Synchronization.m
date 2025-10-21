8ae26a83ff163436a6e270bd8e69089507dee8c2
Васильев Андрей Сергеевич <vasilevs2003@yandex.ru>
2025-10-22 01:51:33 +0300

    Edit MatLab\Main\Slot_Synchronization.m
    Функция дописана за исключением определения порога.

--- MatLab/Main/Slot_Synchronization.m
+++ MatLab/Main/Slot_Synchronization.m
@@ -11,7 +11,6 @@ function Slots_Offsets = Slot_Synchronization(FSignal, Flag_Draw)
 % Выходные переменные:
 %   Slots_Offsets – массив значений сдвигов в FSignal до начала слотов
 %                   найденных сигналов базовых станций.
-Slots_Offsets = [];
 
 % Параметры 
     % Период корреляционных максимумов в отсчётах
@@ -21,6 +20,8 @@ Slots_Offsets = [];
     % Использовать когерентное накопление. В противном случае будет
     % использовано некогерентное
         useKoherent = 1;
+    % Ширина окрестности максимума, которая будет занулена. Чётное число
+        OmitWidth = 48;
 
 % Генерация синхропоследовательности
     PSP = Generate_Primary_Synchronisation_Code;
@@ -53,34 +54,38 @@ Slots_Offsets = [];
             conv(ShapedSignal(:, i), fliplr(conj(PSPZeros)).', "valid");
     end
 
-% Когерентное и некогерентное накопление результата
+% Когерентное накопление результата
     KoherentRes = abs(sum(CorrRes2, 2));
-    NonKoherentRes = sum(abs(CorrRes2), 2);
-
-% Использовать результаты когерентного или некогерентного накопления при
-% дальнейших действиях
-    if useKoherent
-        AccumulationRes = KoherentRes;
-    else
-        AccumulationRes = NonKoherentRes;
-    end
 
 % Прорисовка результата накопления
     if Flag_Draw 
         figure;
-        subplot(2, 1, 1)
         plot(KoherentRes); grid on;
         title('Когерентное накопление');
-        subplot(2, 1, 2)
-        plot(NonKoherentRes); grid on;
-        title('Некогерентное накопление');
-        % figure
-        % histogram(abs(KoherentRes)); figure
-        % histogram(abs(NonKoherentRes));
     end
 
-% ДЗ:
-    % 1. Определить порог
-    % 2. Разобраться с видами накопления
+% Определяем порог различения базовых станций от шума
+    Threshold = 1;
 
-    1
+% Обрабатываем все корреляционные максимумы, превышающие порог
+    Slots_Offsets = [];
+
+    while sum(KoherentRes >= Threshold) > 0
+        % Выбор самого высокого максимума
+            [~, Slots_Offsets(end+1)] = max(KoherentRes);
+            
+        % Зануление выбранного максимума и его окрестностей
+            % Определение границ зоны зануления
+                if Slots_Offsets(end) - OmitWidth/2 < 1
+                    Pos1 = 1;
+                else 
+                    Pos1 = Slots_Offsets(end) - OmitWidth/2;
+                end
+                if Slots_Offsets(end) + OmitWidth/2 > CorrPeriod
+                    Pos2 = CorrPeriod;
+                else 
+                    Pos2 = Slots_Offsets(end) + OmitWidth/2;
+                end
+
+            KoherentRes(Pos1:Pos2) = 0;
+    end
