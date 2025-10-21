9d7b036c34555aed521b0d84bacc24d15c278c73
ans.vasilev <vasilevs2003@yandex.ru>
2025-10-21 14:32:00 +0300

    Edit MatLab\Main\Slot_Synchronization.m
    Переписан код, но задачи стоят всё те же.

--- MatLab/Main/Slot_Synchronization.m
+++ MatLab/Main/Slot_Synchronization.m
@@ -11,43 +11,73 @@ function Slots_Offsets = Slot_Synchronization(FSignal, Flag_Draw)
 % Выходные переменные:
 %   Slots_Offsets – массив значений сдвигов в FSignal до начала слотов
 %                   найденных сигналов базовых станций.
+Slots_Offsets = [];
 
-% Число слотов для накопления сигнала
-    N = 50;
+% Параметры 
+    % Период корреляционных максимумов в отсчётах
+        CorrPeriod = 5120;
+    % Число слотов, используемых для накопления
+        AccumulationSize = 50;
+    % Использовать когерентное накопление. В противном случае будет
+    % использовано некогерентное
+        useKoherent = 1;
 
-% Генерация ПСП
+% Генерация синхропоследовательности
     PSP = Generate_Primary_Synchronisation_Code;
-
-% Вставка нулей
     PSPZeros = upsample(PSP, 2);
+    PSPZeros = PSPZeros(1:end-1);
+    PSPZerosLen = length(PSPZeros);
 
-% Корреляция с ПСП
-    corrRes = conv(FSignal, fliplr(conj(PSPZeros)));
+% Корреляция сигнала с ПСП
+    CorrRes = conv(FSignal, fliplr(conj(PSPZeros)), "valid");
 
-% 
-    InReshape = reshape(FSignal(1:5120*N), 5120, N);
-
-    InReshape2 = [];
-
-    for i = 1:N
-        InReshape2(:, end+1) = FSignal((1:5120+511) + (i-1)*5120);
+% Прорисовка результата корреляции
+    if Flag_Draw 
+        plot(abs(CorrRes)); grid on;
+        title('Корреляция сигнала с ПСП');
     end
 
-    corrRes2 = [];
+% Шейпинг сигнала для накопления
+    ShapedSignal = zeros(CorrPeriod+PSPZerosLen-1, AccumulationSize);
 
-    for i = 1:size(InReshape, 2)
-        corrRes2(:, end+1) = conv(InReshape2(:, i), fliplr(conj(PSPZeros)), "valid");
+    for i = 1:AccumulationSize
+        ShapedSignal(:, i) = ...
+            FSignal((1:CorrPeriod+PSPZerosLen-1) + (i-1)*CorrPeriod).';
     end
 
-% Когерентное накопление
-    Kog = abs(sum(corrRes2, 2));
+% Кореляции с синхропоследовательностью
+    CorrRes2 = zeros(CorrPeriod, AccumulationSize);
 
-% Некогерентное накопление
-    NeKog = sum(abs(corrRes2), 2);
+    for i = 1:AccumulationSize
+        CorrRes2(:, i) = ...
+            conv(ShapedSignal(:, i), fliplr(conj(PSPZeros)).', "valid");
+    end
 
+% Когерентное и некогерентное накопление результата
+    KoherentRes = abs(sum(CorrRes2, 2));
+    NonKoherentRes = sum(abs(CorrRes2), 2);
 
-    plot(Kog); grid on; figure;
-    plot(NeKog); grid on;
+% Использовать результаты когерентного или некогерентного накопления при
+% дальнейших действиях
+    if useKoherent
+        AccumulationRes = KoherentRes;
+    else
+        AccumulationRes = NonKoherentRes;
+    end
+
+% Прорисовка результата накопления
+    if Flag_Draw 
+        figure;
+        subplot(2, 1, 1)
+        plot(KoherentRes); grid on;
+        title('Когерентное накопление');
+        subplot(2, 1, 2)
+        plot(NonKoherentRes); grid on;
+        title('Некогерентное накопление');
+        figure
+        histogram(abs(KoherentRes)); figure
+        histogram(abs(NonKoherentRes));
+    end
 
 % ДЗ:
     % 1. Определить порог
