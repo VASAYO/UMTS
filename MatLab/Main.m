95fc497b115ddbdb3a1e4cfca800f5308516fd26
Васильев Андрей Сергеевич <vasilevs2003@yandex.ru>
2025-10-22 16:09:37 +0300

    ***

--- MatLab/Main.m
+++ MatLab/Main.m
@@ -1,7 +1,7 @@
 % Очистка динамической памяти и Command Window
     clc;
     clear;
-    close all;
+    % close all;
     
 % Добавление путей
     path([cd, '\Signals'], path);
@@ -19,32 +19,32 @@
     FSignal = Matched_Filter(Signal(7:end), 0);
     
 % Слотовая синхронизация - поиск базовых станций (БС)
-    Slots_Offsets = Slot_Synchronization(FSignal, true);
-% % Для каждой найденной БС проводим следующие процедуры обработки
-%     if ~isempty(Slots_Offsets)
-%         % Создадим переменную для хранения транспортных блоков вещательного
-%         % канала найденных БС
-%             BCCHs = cell(length(Slots_Offsets), 1);
-%         for k = 1:1 %length(Slots_Offsets) % Для каждой БС
-%             % Кадровая синхронизация    
-%                 [Frame_Offset, SG] = Frame_Synchronization(FSignal, ...
-%                     Slots_Offsets(1, k), true);
-%             % Определение номера скремблирующей последовательности    
-%                 SC_Num = Scrambling_Code_Determination(FSignal, ...
-%                     Frame_Offset, SG, true);
-%             % Построение rake-шаблона
-%                 Rake_Pattern = Rake_Pattern_Calculation(Signal, ...
-%                     FSignal, Frame_Offset, SC_Num, true);
-%             % Демодуляция вещательного канала
-%                 PCCPCH_Bits = One_Ray_PCCPCH_Demodulation(Signal, ...
-%                     Rake_Pattern, Frame_Offset, SC_Num, true);
-%             % Декодирование транспортных блоков вещательного канала
-%                 [Flag_isOk, BCCH] = Decode_BCCH(PCCPCH_Bits);
-%                 display(Flag_isOk);
-%             % Сохранение полученных данных
-%                 BCCHs{k, 1} = BCCH;
-%         end
-%     end
+    Slots_Offsets = Slot_Synchronization(FSignal, 0);
+% Для каждой найденной БС проводим следующие процедуры обработки
+    if ~isempty(Slots_Offsets)
+        % Создадим переменную для хранения транспортных блоков вещательного
+        % канала найденных БС
+            BCCHs = cell(length(Slots_Offsets), 1);
+        for k = 1:1 %length(Slots_Offsets) % Для каждой БС
+            % Кадровая синхронизация    
+                [Frame_Offset, SG] = Frame_Synchronization(FSignal, ...
+                    Slots_Offsets(1, k), true);
+            % % Определение номера скремблирующей последовательности    
+            %     SC_Num = Scrambling_Code_Determination(FSignal, ...
+            %         Frame_Offset, SG, true);
+            % % Построение rake-шаблона
+            %     Rake_Pattern = Rake_Pattern_Calculation(Signal, ...
+            %         FSignal, Frame_Offset, SC_Num, true);
+            % % Демодуляция вещательного канала
+            %     PCCPCH_Bits = One_Ray_PCCPCH_Demodulation(Signal, ...
+            %         Rake_Pattern, Frame_Offset, SC_Num, true);
+            % % Декодирование транспортных блоков вещательного канала
+            %     [Flag_isOk, BCCH] = Decode_BCCH(PCCPCH_Bits);
+            %     display(Flag_isOk);
+            % % Сохранение полученных данных
+            %     BCCHs{k, 1} = BCCH;
+        end
+    end
     
 % Удаление путей
 %     rmpath([cd, '\Signals']);
