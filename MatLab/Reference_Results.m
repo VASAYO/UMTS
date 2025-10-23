30f62ac8ae60aec9d86189554aec5324f2dfd8aa
Васильев Андрей Сергеевич <vasilevs2003@yandex.ru>
2025-10-23 15:06:36 +0300

    Edit MatLab\Reference_Results.m
    Добавлены комментарии со сравнением результатов слотовой, кадровой синхронизации, а также определения номера скрэмблирующей группы.

--- MatLab/Reference_Results.m
+++ MatLab/Reference_Results.m
@@ -1,47 +1,50 @@
 Beeline
-    Slot_Offset = 3915;
-    Frame_Offset = 3915;
-    SG = 45;
+    Slot_Offset = 3915; % Результат: 3950 (+35)
+    Frame_Offset = 3915; % Результат: 3950 (+35)
+    SG = 45; % Сошлось
     SC_Num = 366;
     Real_Frame_Offset = 3915;
     df = -9.5708e-06; % -37 Гц
     
 Megafon
-    Slot_Offset = 1288;
-    Frame_Offset = 11528;
-    SG = 3;
+    Slot_Offset = 1288; % Результат: 1323 (+35)
+    Frame_Offset = 11528; % Результат: 11563 (+35)
+    SG = 3; % Сошлось
     SC_Num = 24;
     Real_Frame_Offset = 11527;
     df = -1.0510e-05; % -40 Гц
     
 Megafon5
-    Slot_Offset = 916;
-    Frame_Offset = 11156;
-    SG = 3;
+    Slot_Offset = 916; % Результат: 951 (+35)
+    Frame_Offset = 11156; % Результат: 11191 (+35)
+    SG = 3; % Сошлось
     SC_Num = 30;
     Real_Frame_Offset = 11156;
     df = -3.5254e-06; % -14 Гц
     
 Megafon99
-    Slot_Offset = 1044;
-    Frame_Offset = 31764;
-    SG = 25;
+    Slot_Offset = 1044; % Результат: 2667 (+1623). 
+                        % Вторая по мощности базовая станция: 1079 (+35). 
+                        % Дальнейшие результаты приводятся именно для неё.
+                        
+    Frame_Offset = 31764; % Результат: 31799 (+35)
+    SG = 25; % Сошлось
     SC_Num = 204;
     Real_Frame_Offset = 31764;
     df = 3.3755e-06; % 13 Гц
     
 Megafon99_01
-    Slot_Offset = 1044;
-    Frame_Offset = 31764;
-    SG = 25;
+    Slot_Offset = 1044; % Результат: 1079 (+35).
+    Frame_Offset = 31764; % Результат: 31799 (+35)
+    SG = 25; % Сошлось
     SC_Num = 204;
     Real_Frame_Offset = 31764;
     df = 3.9349e-04; % 1 511 Гц
     
 MTS
-    Slot_Offset = 3254;
-    Frame_Offset = 64694;
-    SG = 57;
+    Slot_Offset = 3254; % Результат: 3289 (+35).
+    Frame_Offset = 64694; % Результат: 64729 (+35).
+    SG = 57; % Сошлось
     SC_Num = 461;
     Real_Frame_Offset = 64694;
     df = -1.1231e-05; % -43 Гц
