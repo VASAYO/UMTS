a2945c1be1d33f440a08d2383cf1a3e588b6582d
Васильев Андрей Сергеевич <vasilevs2003@yandex.ru>
2025-11-12 20:27:31 +0300

    ***

--- MatLab/Main.m
+++ MatLab/Main.m
@@ -10,7 +10,7 @@
     
 % Загрузка массива Signal с записью сигнала    
     % Beeline Megafon Megafon5 Megafon99 Megafon99_01 MTS   
-    load('MTS');    % В кавычках указывается имя файла из
+    load('Beeline');    % В кавычках указывается имя файла из
                     % которого будет считана запись сигнала
 % Удаление отсчётов сигнала с артефактами
     Signal = Signal(7:end);
@@ -29,7 +29,7 @@
         % канала найденных БС
             BCCHs = cell(length(Slots_Offsets), 1);
 
-        for k = 1:1 %length(Slots_Offsets) % Для каждой БС
+        for k = 1:1%length(Slots_Offsets) % Для каждой БС
             % Кадровая синхронизация    
                 [Frame_Offset, SG] = Frame_Synchronization(FSignal, ...
                     Slots_Offsets(1, k), 0);
@@ -42,14 +42,14 @@
                 Rake_Pattern = Rake_Pattern_Calculation(Signal, ...
                     FSignal, Frame_Offset, SC_Num, 0);
 
-            % % Демодуляция вещательного канала
-            %     PCCPCH_Bits = One_Ray_PCCPCH_Demodulation(Signal, ...
-            %         Rake_Pattern, Frame_Offset, SC_Num, true);
-            % % Декодирование транспортных блоков вещательного канала
-            %     [Flag_isOk, BCCH] = Decode_BCCH(PCCPCH_Bits);
-            %     display(Flag_isOk);
-            % % Сохранение полученных данных
-            %     BCCHs{k, 1} = BCCH;
+            % Демодуляция вещательного канала
+                PCCPCH_Bits = One_Ray_PCCPCH_Demodulation(Signal, ...
+                    Rake_Pattern, Frame_Offset, SC_Num, 0);
+            % Декодирование транспортных блоков вещательного канала
+                [Flag_isOk, BCCH] = Decode_BCCH(PCCPCH_Bits);
+                display(Flag_isOk);
+            % Сохранение полученных данных
+                BCCHs{k, 1} = BCCH;
         end
     end
 
